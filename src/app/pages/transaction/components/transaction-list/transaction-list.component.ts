758b3a2abe213ba427ef17fe921fcc32388259df
Pavel Pazderník <pazdernik.pavel@email.cz>
2024-03-17 17:31:47 +0100

    Add transaction search

--- src/app/pages/transaction/components/transaction-list/transaction-list.component.ts
+++ src/app/pages/transaction/components/transaction-list/transaction-list.component.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Signal, inject, viewChild } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import {
   NbButtonModule,
+  NbFormFieldModule,
   NbIconModule,
+  NbInputModule,
   NbPopoverDirective,
   NbPopoverModule,
   NbSpinnerModule,
 } from '@nebular/theme';
 import { Store } from '@ngrx/store';
+import { combineLatest, debounceTime, map, startWith } from 'rxjs';
 
 import { TransactionType } from '@app/graphql/generated/schema';
 import { selectCategoryById } from '@app/shared/modules/category/store/category.selectors';
@@ -30,9 +35,13 @@ import {
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     NbSpinnerModule,
     NbIconModule,
     NbButtonModule,
+    NbInputModule,
+    NbFormFieldModule,
+    NbIconModule,
     NbPopoverModule,
     TableModule,
     PopoverConfirmationComponent,
@@ -41,10 +50,29 @@ import {
 export class TransactionListComponent {
   private store = inject(Store);
 
+  public readonly searchControl = new FormControl('');
   public readonly popover = viewChild.required(NbPopoverDirective);
   public readonly transactionType = TransactionType;
-  public readonly transactions = this.store.selectSignal(selectTransactionList);
   public readonly isFetchingList = this.store.selectSignal(selectIsFetchingTransactionList);
+
+  public readonly transactions = toSignal(
+    combineLatest({
+      transactions: this.store.select(selectTransactionList),
+      search: this.searchControl.valueChanges.pipe(debounceTime(150), startWith('')),
+    }).pipe(
+      map(({ transactions, search }) =>
+        search
+          ? transactions.filter(
+              transaction =>
+                transaction.title.toLowerCase().includes(search.toLowerCase()) ||
+                (transaction.description &&
+                  transaction.description.toLowerCase().includes(search.toLowerCase())),
+            )
+          : transactions,
+      ),
+    ),
+    { initialValue: [] },
+  );
 
   constructor() {
     this.store.dispatch(getTransactionsActions.start());
